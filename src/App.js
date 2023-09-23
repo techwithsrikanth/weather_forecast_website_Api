73fd5cfa5a78a03075b59cd33193d5a55cc5f3d1
Srikanthan <srikanthprakash072003@gmail.com>
2023-09-24 01:48:22 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const apiKey = '28e3ddcaff98f1ee06f08ee5df52acd0';
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState({});
+  const [city, setCity] = useState('');
+  const [error, setError] = useState(null);
+
+  const getWeather = () => {
+    // Check if the city is not empty before making the API request
+    if (city.trim() === '') {
+      setError('Please enter a city name');
+      return;
+    }
+
+    // Fetch weather data based on the city
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.cod === 200) {
+          setWeatherData(data);
+          setError(null);
+        } else {
+          setError(`City not found: ${data.message}`);
+          setWeatherData({});
+        }
+      })
+      .catch(error => {
+        setError('An error occurred while fetching weather data');
+        setWeatherData({});
+      });
+  }
+
+  return (
+    <div className='container'>
+      <h1>Weather App</h1>
+      <div className='input'>
+        <input
+          type='text'
+          placeholder='Enter city name'
+          value={city}
+          onChange={e => setCity(e.target.value)}
+        />
+        <button onClick={getWeather}>Get Weather</button>
+      </div>
+      {error && <p className='error'>{error}</p>}
+      {weatherData.main && (
+        <div className='weather'>
+          <h2>Weather in {weatherData.name}, {weatherData.sys.country}</h2>
+          <p>Temperature: {weatherData.main.temp}&deg;F</p>
+          <p>Humidity: {weatherData.main.humidity}%</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
